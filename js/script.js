ff12d9b1bd8cba327f3f69af62e3d18c592e7611
muratkilic1978 <muratkilic1978@gmail.com>
2020-02-05 21:18:21 +0100

    script.js was updated

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ const showNumber = document.getElementById("showNumber");
 const btnNumber = document.querySelector(".btnNumber");
 btnNumber.addEventListener("click",function(){
     
-    let randomNumber = Math.floor((Math.random() * 100) +1);
-    console.log(randomNumber);
-    showNumber.innerHTML = randomNumber;
+    let randomNumberR =  Math.floor(Math.random() * 256);
+    console.log(randomNumberR);
+    let randomNumberG =  Math.floor(Math.random() * 256);
+    console.log(randomNumberG);
+    let randomNumberB =  Math.floor(Math.random() * 256);
+    console.log(randomNumberB);
+    
+    document.body.style.backgroundColor = "RGB("+randomNumberR+","+randomNumberG+","+randomNumberB+")";
+    
+    showNumber.innerHTML = "RGB("+randomNumberR+","+randomNumberG+","+randomNumberB+")";
 });
-
 
 
